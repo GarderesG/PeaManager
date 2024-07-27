7d5882e2a654bb3562cd86b5e642179b95f4ebf4
Guillaume Garderes <= g.garderes@gmail.com>
2024-07-27 17:33:14 +0200

    Put SECRET_KEY in environmental variables before releasing to Github

--- pea_project/settings.py
+++ pea_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k)$co%cn4j8^d!mfr@&efr0)ofon2fz+*z0z%b_7hy6bxmf0v$'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
